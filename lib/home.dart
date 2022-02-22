2e4a21645cd7dd12a0412a203fd7ee0b5b680526
Justin-roy <roysunny951@gmail.com>
2022-02-22 17:37:55 +0530

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,101 @@
+// ignore_for_file: avoid_print
+
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:path/path.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // Getting File From Storage...
+  File? file;
+  Future selectFile() async {
+    final result = await FilePicker.platform.pickFiles();
+    if (result == null) return;
+    final path = result.files.single.path!;
+    setState(() {
+      file = File(path);
+    });
+  }
+
+  // Uploading File To Firebase Storage
+  Future<void> uploadFile() async {
+    final filename = basename(file!.path);
+    try {
+      await FirebaseStorage.instance.ref('files/$filename').putFile(file!);
+    } on FirebaseException catch (e) {
+      print(e);
+    }
+  }
+
+  // Downloading Url
+  Future<void> downloadURL() async {
+    final filename = basename(file!.path);
+    String downloadURL =
+        await FirebaseStorage.instance.ref('files/$filename').getDownloadURL();
+
+    print('Download URL:- $downloadURL');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final filename = file != null ? basename(file!.path) : 'No File Selected';
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HomePage'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(),
+            SizedBox(
+              height: 60,
+              width: MediaQuery.of(context).size.width / 1.2,
+              child: ElevatedButton(
+                onPressed: selectFile,
+                child: const Text('Select File'),
+              ),
+            ),
+            const SizedBox(height: 10),
+            SizedBox(
+              height: 50,
+              width: MediaQuery.of(context).size.width / 1.2,
+              child: Text(
+                filename,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(
+              height: 60,
+              width: MediaQuery.of(context).size.width / 1.2,
+              child: ElevatedButton(
+                onPressed: () async {
+                  await uploadFile();
+                  // Showing message to user
+                  final snackBar = SnackBar(
+                    content: const Text('File Uploaded To Firebase!'),
+                    action: SnackBarAction(
+                      label: 'Undo',
+                      onPressed: () {},
+                    ),
+                  );
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                  downloadURL();
+                },
+                child: const Text('Upload File'),
+              ),
+            ),
+            const Spacer(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:cloud_firebase_storage/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Firebase Storage',
+      theme: ThemeData(
+          primarySwatch: Colors.blue,
+          appBarTheme: const AppBarTheme(
+            centerTitle: true,
+          )),
+      home: const HomePage(),
+    );
+  }
+}
